26d1d57f691ca14a1e33450937280bcb97e5199d
sknop8 <sknop8@gmail.com>
2016-02-20 16:47:28 -0500

    asf

---
+++ decisionmaker/src/Node.java
@@ -0,0 +1,21 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.*;
+
+public class Node extends JButton {
+	
+	public Node(String option1, String option2) {
+		
+	}
+	
+	public void spin() {
+		
+	}
+
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.BLACK);
+		
+	}
+}
