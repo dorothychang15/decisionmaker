659c3313fdd6e404eda2a0c3afaa2f4faaf084b4
sknop8 <sknop8@gmail.com>
2016-02-20 16:22:08 -0500

    node

--- decisionmaker/src/decisionmaker/Node.java
+++ decisionmaker/src/decisionmaker/Node.java
@@ -2,11 +2,17 @@ package decisionmaker;
 import javax.swing.*;
 
 public class Node extends JButton {
+	
 	public Node(String option1, String option2) {
 		
 	}
 	
 	public void spin() {
+		
+	}
+
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.BLACK);
 		
 	}
 }
